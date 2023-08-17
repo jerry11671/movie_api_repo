4da6dee1bd0712473b02fc375625244cad827249
Jerry <chiemenagodson532@gmail.com>
2023-08-17 05:38:37 +0100

    Update with jwt authentication

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from .views import register, logout_view
 
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     
     path('register/', register, name='register'),
